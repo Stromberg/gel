506ca5e3a2d15a90c6d369140e16cc507ecc3826
Johannes Strömberg <johannes.stromberg@gmail.com>
2019-06-03 19:09:42 +0200

    gel repl

--- gel.go
+++ gel.go
@@ -2,7 +2,14 @@
 package gel
 
 import (
+	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strings"
+
 	"github.com/Stromberg/gel/ast"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // Gel is the language expression handler.
@@ -32,19 +39,94 @@ func (g *Gel) Code() string {
 // Missing returns the symbols that are missing
 // in the environment in order to evaluate the expression.
 func (g *Gel) Missing(env *Env) []string {
-	scope := NewScope(g.fset, g.modules...)
-
-	env.fillScope(scope)
-
+	scope := g.scope(env)
 	return missing(scope, g.node)
 }
 
 // Eval evaluates the expression in the given environment.
 func (g *Gel) Eval(env *Env) (interface{}, error) {
-	scope := NewScope(g.fset, g.modules...)
-
-	env.fillScope(scope)
+	scope := g.scope(env)
 	return scope.Eval(g.node)
+}
+
+func (g *Gel) scope(env *Env) *Scope {
+	scope := NewScope(g.fset, g.modules...)
+	env.fillScope(scope)
+	return scope
+}
+
+func (g *Gel) Repl(env *Env) error {
+	scope := g.scope(env)
+
+	state, err := terminal.MakeRaw(1)
+	if err != nil {
+		return err
+	}
+	defer terminal.Restore(1, state)
+
+	t := terminal.NewTerminal(os.Stdout, "> ")
+	unclosed := ""
+	for {
+		line, err := t.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		if line == "exit" {
+			break
+		}
+		if unclosed != "" {
+			line = unclosed + "\n" + line
+		}
+		unclosed = ""
+		t.SetPrompt("> ")
+		node, err := ParseString(g.fset, "", line)
+		if err != nil {
+			if strings.HasSuffix(err.Error(), "missing )") {
+				unclosed = line
+				t.SetPrompt(". ")
+				continue
+			}
+			fmt.Println(err)
+			continue
+		}
+		value, err := scope.Eval(node)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if value != nil {
+			if reflect.TypeOf(value).Kind() == reflect.Func {
+				fmt.Println("#func")
+			} else if v, ok := value.([]interface{}); ok {
+				if len(v) == 0 {
+					fmt.Println("()")
+				} else {
+					fmt.Print("(list")
+					for _, e := range v {
+						fmt.Printf(" %#v", e)
+					}
+					fmt.Println(")")
+				}
+			} else if v, ok := value.([]float64); ok {
+				if len(v) == 0 {
+					fmt.Println("()")
+				} else {
+					fmt.Print("(vec")
+					for _, e := range v {
+						fmt.Printf(" %#v", e)
+					}
+					fmt.Println(")")
+				}
+			} else {
+				fmt.Printf("%#v\n", value)
+			}
+		}
+	}
+	fmt.Println()
+	return nil
 }
 
 func missing(s *Scope, node ast.Node) []string {
